455e588e684aea27b4e9e9447591d0a48caa473d
Calvin Cheng <linchuan.cheng@gmail.com>
2015-03-08 11:14:38 +0800

    CoreLocation

--- Where Am I/ViewController.swift
+++ Where Am I/ViewController.swift
@@ -7,14 +7,67 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet var latitude: UILabel!
+    @IBOutlet var longitude: UILabel!
+    @IBOutlet var heading: UILabel!
+    @IBOutlet var speed: UILabel!
+    @IBOutlet var altitude: UILabel!
+    @IBOutlet var address: UILabel!
+    
+    var manager:CLLocationManager!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        manager = CLLocationManager()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
     }
 
+    func locationManager(manager:CLLocationManager, didUpdateLocations locations:[AnyObject]) {
+        
+        var userLocation:CLLocation = locations[0] as CLLocation
+        
+        print(userLocation)
+        
+        latitude.text = "\(userLocation.coordinate.latitude)"
+        longitude.text = "\(userLocation.coordinate.longitude)"
+        heading.text = "\(userLocation.course)"
+        speed.text = "\(userLocation.speed)"
+        altitude.text = "\(userLocation.altitude)"
+        
+        // reverse geocoding to get address
+        CLGeocoder().reverseGeocodeLocation(userLocation, completionHandler:{(placemarks, error) in
+        
+            if error != nil {
+                println(error)
+            } else {
+                println(placemarks)
+                let p = CLPlacemark(placemark: placemarks?[0] as CLPlacemark)
+                
+                var subThoroughfare:String
+                if (p.subThoroughfare != nil) {
+                    subThoroughfare = p.subThoroughfare
+                } else {
+                    subThoroughfare = ""
+                }
+                
+                self.address.text = "\(subThoroughfare) \(p.thoroughfare) \n \(p.subLocality) \n \(p.subAdministrativeArea) \n \(p.postalCode)"
+
+            }
+        
+        })
+
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
